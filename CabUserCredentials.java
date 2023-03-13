109304c44cecfe5e56529332b819246bb38a328b
PavaniYama <126776622+PavaniYama@users.noreply.github.com>
2023-03-13 09:51:16 +0530

    Add files via upload

---
+++ CabUserCredentials.java
@@ -0,0 +1,11 @@
+package cabBooking;
+import java.util.Scanner;
+public class CabUserCredentials  {
+public static void main (String[]args) {
+	
+		Scanner scanner=new Scanner (System.in);
+		CabRequest cabrequest=new CabRequest(100);
+		cabrequest.userLogin();
+		
+		}	
+}	
